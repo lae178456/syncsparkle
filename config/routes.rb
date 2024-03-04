b8de112178101a514a23c66798ecc7464df9dbbb
Asha Kiran Devkota <aashahope01@gmail.com>
2024-03-04 19:25:54 +0100

    Routes added

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
+  get 'events/index'
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   resources :pages, only: [:home] # Assuming you have a PagesController with a home action
   resources :events, only: [:new, :create, :index, :edit, :update, :destroy, :show]
+
+
+
 
   resources :users, only: [:show, :edit, :update] do
     resource :profile, only: [:edit, :update] # Nested resource for user profiles
@@ -17,3 +21,17 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
 end
+
+
+
+
+
+
+# Define custom routes for event categories
+# get '/events/dashboard', to: 'events#dashboard', as: 'events_dashboard'
+#get 'events/draft', to: 'events#draft', as: 'events_draft'
+#get 'events/to_be_modified', to: 'events#to_be_modified', as: 'events_to_be_modified'
+#get 'events/under_review', to: 'events#under_review', as: 'events_under_review'
+#get 'events/published', to: 'events#published', as: 'events_published'
+#get 'events/cancelled', to: 'events#cancelled', as: 'events_cancelled'
+#get 'events/archived', to: 'events#archived', as: 'events_archived'
