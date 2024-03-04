84c69a56eb6a2541c16215fb419c111214117087
Asha Kiran Devkota <aashahope01@gmail.com>
2024-03-04 09:33:40 +0100

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,13 @@ Rails.application.routes.draw do
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  resources :pages, only: [:home] # Assuming you have a PagesController with a home action
+  resources :events, only: [:new, :create, :index, :edit, :update, :destroy, :show]
+
+  resources :users, only: [:show, :edit, :update] do
+    resource :profile, only: [:edit, :update] # Nested resource for user profiles
+  end
+
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
