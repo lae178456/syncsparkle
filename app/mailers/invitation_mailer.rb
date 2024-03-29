aa150215d7424f582ebbe176baef8563edca2383
Asha Kiran Devkota <aashahope01@gmail.com>
2024-03-29 19:08:38 +0100

    reverted wicked html to pdf files

--- app/mailers/invitation_mailer.rb
+++ app/mailers/invitation_mailer.rb
@@ -6,21 +6,9 @@ class InvitationMailer < ApplicationMailer
   #   en.invitation_mailer.invitation.subject
   #
   def invitation
-      @event = params[:event]
-      @event_url = params[:event_url]
-      @recipient_email = params[:recipient_email]
-
-      # Generate the PDF content
-      attachments['attachment.pdf'] = WickedPdf.new.pdf_from_string(
-        render_to_string(pdf: "invitation", template: 'events/invitation', formats: [:html])
-      )
-
-
-    # pdf_content = render_to_string(pdf: "event_details", template: "events/show.pdf.erb", layout: false, encoding: "UTF-8")
-
-    # Attach the PDF to the email
-    # attachments["event_details.pdf"] = pdf_content
-
-      mail(to: @recipient_email, subject: 'Invitation to Event')
+    @event = params[:event]
+    @event_url = params[:event_url]
+    @recipient_email = params[:recipient_email]
+    mail(to: @recipient_email, subject: 'Invitation to Event')
   end
 end
--- config/initializers/wicked_pdf.rb
+++
@@ -1,9 +0,0 @@
-WickedPdf.config ||= {}
-# WickedPdf.config.merge!({
-#   layout: "pdf.html.erb",
-# })
-
-# WickedPdf.configure do |c|
-#   c.exe_path = '/usr/local/bin/wkhtmltopdf'
-#   c.enable_local_file_access = true
-# end
